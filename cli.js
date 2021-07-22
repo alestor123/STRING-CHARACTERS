e49545c77ee0e2a3502155c7e1cc9241aad467c1
alestor123 <alestoraldous@gmail.com>
2021-07-22 19:46:54 +0530

    added main cli

--- cli.js
+++ cli.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+const stringChar = require('./App')
+const { readFileSync } = require('fs')
+const chalk = require('chalk')
+
+try {
+  console.log(stringChar(readFileSync(process.argv[2]).toString()).sort().join())
+} catch (e) {
+  console.log(chalk.redBright.bold('Oops : ' + e.message))
+}
